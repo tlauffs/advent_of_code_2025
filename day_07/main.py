cffe0e0524b9bd6e3898f19b48ce2cb3687603c8
tlauffs <timlauffs@proton.me>
2025-12-07 22:10:00 +0100

    balls

--- day_07/main.py
+++ day_07/main.py
@@ -1,8 +1,12 @@
+from collections import defaultdict
+from typing import DefaultDict
+
 with open("input.txt") as f:
     lines = f.readlines()
 
 start_col: int = lines[0].strip().index("S")
-beam_cols: dict[int, int] = {start_col: 1}
+beam_cols: DefaultDict[int, int] = defaultdict(int)
+beam_cols[start_col] = 1
 split_count: int = 0
 
 for line in lines[1:]:
@@ -12,11 +16,7 @@ for line in lines[1:]:
             split_count += 1
             current_count = beam_cols[beam_col]
             beam_cols.pop(beam_col)
-            if not beam_col + 1 in beam_cols:
-                beam_cols[beam_col + 1] = 0
             beam_cols[beam_col + 1] += current_count
-            if not beam_col - 1 in beam_cols:
-                beam_cols[beam_col - 1] = 0
             beam_cols[beam_col - 1] += current_count
 
 print(f"Part 1: {split_count}")
